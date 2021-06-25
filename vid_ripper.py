d04e7a15349decfa1c51b0fda7ddc1c2d895ffc4
JackFarmer132 <jackfarmer858@gmail.com>
2021-06-25 20:24:09 +0100

    first version of rupper working, generated clips from the source file

--- vid_ripper.py
+++ vid_ripper.py
@@ -1,21 +1,38 @@
 from conf import *
+from clip_times import *
+
 import math
+import random
+import string
+
 from moviepy.editor import *
 from PIL import Image
 from moviepy.video.fx.all import crop
 
-try:
-    # parameters: enter the source file name, beginning of clip and end of clip
-    source_name = str(sys.argv[1]) + ".mp4"
-    clip_start = int(sys.argv[2])
-    clip_end = int(sys.argv[3])
+# runs the ripper using the given clip times to generate all clips from input
+def rip_looper():
+    # for each source video
+    for source in clip_times:
+        # get list of timings for clipping
+        for ct in clip_times[source]:
+            source_name = source
+            start_time = ct[0]
+            end_time = ct[1]
+            ripper(source_name, start_time, end_time)
+    print("all done :)")
 
+def ripper(source_name, clip_start, clip_end):
     #path of the selected mp4 video
     source_path = os.path.join(SOURCE_DIR, source_name)
-    clip_path = os.path.join(CLIPS_DIR, "clip.mp4")
+    fname = file_name_generator() + ".mp4"
+    clip_path = os.path.join(CLIPS_DIR, fname)
 
     clip = VideoFileClip(source_path)
     subclip = clip.subclip(clip_start, clip_end)
     subclip.write_videofile(clip_path)
-except:
-    print("you probs forgot to add the params for clipping")
+
+# makes random names for clips since they don't matter
+def file_name_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+rip_looper()
