fefb9061cd099345c0190d9a535cf46ce96ff559
JackFarmer132 <jackfarmer858@gmail.com>
2021-06-25 19:37:01 +0100

    thing can now take in args on the vid name and start/end points and save a subclip

--- vid_ripper.py
+++ vid_ripper.py
@@ -1,36 +1,21 @@
 from conf import *
 import math
-from moviepy.editor import VideoFileClip as vfc
+from moviepy.editor import *
 from PIL import Image
+from moviepy.video.fx.all import crop
 
-#path of the selected mp4 video
-source_path = os.path.join(SOURCE_DIR, "sample1.mp4")
+try:
+    # parameters: enter the source file name, beginning of clip and end of clip
+    source_name = str(sys.argv[1]) + ".mp4"
+    clip_start = int(sys.argv[2])
+    clip_end = int(sys.argv[3])
 
-clip = vfc(source_path)
+    #path of the selected mp4 video
+    source_path = os.path.join(SOURCE_DIR, source_name)
+    clip_path = os.path.join(CLIPS_DIR, "clip.mp4")
 
-duration = math.floor(clip.duration)
-
-for i, frame in enumerate(clip.iter_frames()):
-    new_img = Image.fromarray(frame)
-    output_path = os.path.join(OUTPUT_DIR, f"{i}.jpg")
-    new_img.save(output_path)
-
-#destination for clips
-# dest_path = CLIPS_DIR
-# os.makedirs(dest_path, exist_ok=True)
-#
-# clip = VideoFileClip(source_path)
-#
-# print(clip.reader.fps)
-# print(clip.reader.nframes)
-#
-# #turning frames into images
-# duration = clip.reader.duration
-# duration = int(duration) + 1
-#
-# for i in range(0, 10):
-#     print(f"frame at {i} seconds")
-#     frame = clip.get_frame(int(i))
-#     new_img = Image.fromarray(frame)
-#     new_img_filepath = os.path.join(dest_path, f"img{i}.jpg")
-#     new_img.save(new_img_filepath)
+    clip = VideoFileClip(source_path)
+    subclip = clip.subclip(clip_start, clip_end)
+    subclip.write_videofile(clip_path)
+except:
+    print("you probs forgot to add the params for clipping")
