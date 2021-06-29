88948a23ee5b3d8522dc1fd4798db67eb8b0fc2a
JackFarmer132 <jackfarmer858@gmail.com>
2021-06-29 12:54:46 +0100

    optimised struct maker so remaking when new clips are added isn't expensive

--- conf.py
+++ conf.py
@@ -6,3 +6,5 @@ PARENT_DIR = os.path.join(BASE_DIR, "../")
 SOURCE_DIR = os.path.join(BASE_DIR, "../sources")
 CLIPS_DIR = os.path.join(BASE_DIR, "../clips")
 OUTPUT_DIR = os.path.join(BASE_DIR, "../results")
+
+SAT_CLIP_PKL = os.path.join(PARENT_DIR, "sat_clips.pkl")
--- maker.py
+++ maker.py
@@ -15,21 +15,19 @@ def struct_maker():
     # for each clip in the folder
     for fname in os.listdir(CLIPS_DIR):
         path = os.path.join(CLIPS_DIR, fname)
-        clip = VideoFileClip(path)
-        # filename, filepath, duration
-        entry = (fname, path, clip.duration)
+        # filename, filepath
+        entry = (fname, path)
         newlist.append(entry)
-
+    # shuffle, baby!
+    random.shuffle(newlist)
     # save with pickle
-    pickle_path = os.path.join(PARENT_DIR, "sat_list.pkl")
-    with open(pickle_path, 'wb') as f:
+    with open(SAT_CLIP_PKL, 'wb') as f:
         pickle.dump(newlist, f)
 
 # make video with clips with target duration of 10 minutes
 def make_vid():
     # read in list
-    pickle_path = os.path.join(PARENT_DIR, "sat_list.pkl")
-    with open(pickle_path, 'rb') as f:
+    with open(SAT_CLIP_PKL, 'rb') as f:
         clip_list = pickle.load(f)
 
     # reading from head, construct video until duration is 10 minutes
@@ -38,13 +36,13 @@ def make_vid():
     # holds current duration in seconds
     cur_dur = 0
 
-    while (cur_dur < 600):
+    while (cur_dur < 30):
         # get front clip
         clip_package = clip_list.pop(0)
-        # add duration to runtime
-        cur_dur += clip_package[2]
         # generate clip from package
         clip = VideoFileClip(clip_package[1])
+        # add duration to runtime
+        cur_dur += clip.duration
         vid_clips.append(clip)
         used_packages.append(clip_package)
 
@@ -60,7 +58,7 @@ def make_vid():
     clip_list += used_packages
 
     # save with pickle
-    with open(pickle_path, 'wb') as f:
+    with open(SAT_CLIP_PKL, 'wb') as f:
         pickle.dump(clip_list, f)
 
 # makes random names for clips since they don't matter
