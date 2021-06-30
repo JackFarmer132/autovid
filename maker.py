064befba8ee58de2208d879856efcc7791db4648
JackFarmer132 <jackfarmer858@gmail.com>
2021-06-30 15:11:53 +0100

    first vid made, need to adjust the size of the borders and add some shadow but ver1 is basically done

--- maker.py
+++ maker.py
@@ -10,27 +10,26 @@ from moviepy.editor import *
 from PIL import Image
 
 # use this if you need to remake the list if clips folder is updated
-def struct_maker(medium):
-    # if updating video clip struct, say as such (else audio)
-    if (medium == "VID"):
-        directory = CLIPS_DIR
-        pkl_path = SAT_CLIP_PKL
-    else:
-        directory = AUDIO_DIR
-        pkl_path = SAT_AUD_PKL
+def struct_maker():
+    directories = [CLIPS_DIR, AUDIO_DIR, BACKGROUND_DIR]
+    pickle_paths = [SAT_CLIP_PKL, SAT_AUD_PKL, SAT_BCK_PKL]
 
-    newlist = []
-    # for each clip in the folder
-    for fname in os.listdir(directory):
-        path = os.path.join(directory, fname)
-        # filename, filepath
-        entry = (fname, path)
-        newlist.append(entry)
-    # shuffle, baby!
-    random.shuffle(newlist)
-    # save with pickle
-    with open(pkl_path, 'wb') as f:
-        pickle.dump(newlist, f)
+    for i in range(3):
+        newlist = []
+        directory = directories[i]
+        pkl_path = pickle_paths[i]
+
+        # for each clip in the folder
+        for fname in os.listdir(directory):
+            path = os.path.join(directory, fname)
+            # filename, filepath
+            entry = (fname, path)
+            newlist.append(entry)
+        # shuffle, baby!
+        random.shuffle(newlist)
+        # save with pickle
+        with open(pkl_path, 'wb') as f:
+            pickle.dump(newlist, f)
 
 
 # make video with clips with target duration of 10 minutes
@@ -43,6 +42,10 @@ def make_vid():
     with open(SAT_AUD_PKL, 'rb') as f:
         aud_list = pickle.load(f)
 
+    # read in background list
+    with open(SAT_BCK_PKL, 'rb') as f:
+        bck_list = pickle.load(f)
+
     # reading from head, construct video until duration is 10 minutes
     vid_clips = []
     aud_clips = []
@@ -53,7 +56,7 @@ def make_vid():
     aud_dur = 0
 
     # constructs video of at least 10 minutes
-    while (vid_dur < 20):
+    while (vid_dur < 600):
         # get front clip
         clip_package = vid_list.pop(0)
         # generate clip from package
@@ -62,6 +65,9 @@ def make_vid():
         vid_dur += clip.duration
         vid_clips.append(clip)
         used_vid_packages.append(clip_package)
+
+    # make video
+    output_vid = concatenate_videoclips(vid_clips, method="compose")
 
     # constructs audio backing that fits at least the vid length
     while (aud_dur < vid_dur):
@@ -72,9 +78,20 @@ def make_vid():
         aud_clips.append(aud_clip)
         used_aud_packages.append(clip_package)
 
-    # make video and have fadeout
-    output_vid = concatenate_videoclips(vid_clips, method="compose")
+
+    # get background
+    random.shuffle(bck_list)
+    background_choice = bck_list[0]
+    background_vid = VideoFileClip(background_choice[1])
+    background_vid = background_vid.set_duration(vid_dur)
+    background_vid = background_vid.resize(newsize=output_vid.size)
+    left_side = background_vid.set_position((-1400, 0))
+    right_side = background_vid.set_position((1400, 0))
+
+    # overlay videos and add fadeout
+    output_vid = CompositeVideoClip([output_vid, left_side, right_side])
     output_vid = output_vid.fadeout(1)
+
     # make audio
     output_aud = concatenate_audioclips(aud_clips)
     # trim audio to fit video and re-add fade-out
@@ -83,7 +100,6 @@ def make_vid():
 
     # combine video and audio
     output_vid.audio = output_aud
-
     output_name = "sat_" + file_name_generator() + ".mp4"
     output_path = os.path.join(OUTPUT_DIR, output_name)
     output_vid.write_videofile(output_path)
@@ -109,6 +125,5 @@ def file_name_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-
-
+# struct_maker()
 make_vid()
