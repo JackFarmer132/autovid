23500df08c241cb90032cdb839bcc6a7cc7a1879
JackFarmer132 <jackfarmer858@gmail.com>
2021-07-08 13:54:18 +0100

    adding manual upload function in case Google declines ability to automatically upload public videos

--- uploader.py
+++ uploader.py
@@ -1,4 +1,5 @@
 import datetime
+import webbrowser
 
 from conf import *
 from maker import *
@@ -6,39 +7,48 @@ from maker import *
 from Google import Create_Service
 from googleapiclient.http import MediaFileUpload
 
-API_NAME = "youtube"
-API_VERSION = "v3"
-SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+def auto_upload():
+    API_NAME = "youtube"
+    API_VERSION = "v3"
+    SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
-upload_date_time = datetime.datetime(2021, 7, 6, 15, 0, 0).isoformat() + ".000Z"
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-title, upload_vid, upload_thumbnail = make_vid()
+    upload_date_time = datetime.datetime(2021, 7, 6, 15, 0, 0).isoformat() + ".000Z"
 
-request_body = {
-    "snippet": {
-        "title": title,
-        "description": "hello world desc",
-        "tags": ["Travel"]
-    },
-    "status": {
-        "privacyStatus": "private",
-        "publishAt": upload_date_time,
-        "selfDeclareMadeForKids": False,
-    },
-    "notifySubscribers": False
-}
+    title, upload_vid, upload_thumbnail = make_vid()
 
-media_file = MediaFileUpload(upload_vid)
+    request_body = {
+        "snippet": {
+            "title": title,
+            "description": "Welcome to Simply Satisfying! \n\nHere we post the most satisfying content we can find! \nPlease leave a like and let us know what you thought of the video!",
+            "tags": ["Entertainment"]
+        },
+        "status": {
+            "privacyStatus": "private",
+            "publishAt": upload_date_time,
+            "selfDeclareMadeForKids": False,
+        },
+        "notifySubscribers": False
+    }
 
-response_upload = service.videos().insert(
-    part='snippet,status',
-    body=request_body,
-    media_body=media_file
-).execute()
+    media_file = MediaFileUpload(upload_vid)
 
-service.thumbnails().set(
-    videoId=response_upload.get("id"),
-    media_body=MediaFileUpload(upload_thumbnail)
-).execute()
+    response_upload = service.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=media_file
+    ).execute()
+
+    service.thumbnails().set(
+        videoId=response_upload.get("id"),
+        media_body=MediaFileUpload(upload_thumbnail)
+    ).execute()
+
+
+def manual_upload():
+    make_vid()
+    webbrowser.open('https://studio.youtube.com/channel/UCK5aZ60crdCqVsOttPPlkJQ/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D', new=2)
+
+manual_upload()
