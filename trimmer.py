08165df6ba299e36fa014dc97e8297bf8b113586
JackFarmer132 <jackfarmer858@gmail.com>
2021-08-19 17:13:07 +0100

    started integrating new trimming with existing vid maker, should have new version working by the start of the next cycle

--- trimmer.py
+++ trimmer.py
@@ -31,7 +31,7 @@ def findBorderLength(image):
                 dist = math.sqrt((r1 - r2)**2 + (g1 - g2) ** 2 + (b1 - b2) **2)
                 # print(str(pixel_colours[-1]) + ", " + str(dist))
                 # if above theshold for difference, border is over
-                if dist > 10:
+                if dist > 5:
                     break
                 # keep the array small for good averages
                 if len(pixel_colours) > 10:
@@ -40,21 +40,18 @@ def findBorderLength(image):
                 pixel_colours.append(image.getpixel((x,y)))
         # add for safety (better to trim too much than too little)
         x += 10
-        cur_border = max(cur_border, x)
-
-    # if this is the case then it's likely an error occured and default border should be used
-    if cur_border >= 656:
-        cur_border = 0
+        if x < 656:
+            cur_border = max(cur_border, x)
     return cur_border
 
 
 def getBorders(video):
     # get first frame and blur it
     base_frame = Image.fromarray(cv2.blur(video.get_frame(0), (15,15)))
+    base_frame.save(os.path.join(TEMP_CLIPS, "zzzzzzzz/base.jpg"))
     left_border = findBorderLength(base_frame)
     base_frame = base_frame.transpose(Image.FLIP_LEFT_RIGHT)
     right_border = findBorderLength(base_frame)
-
     # fix any errors
     if (not left_border) and (not right_border):
         left_border = 500
@@ -68,16 +65,59 @@ def getBorders(video):
     if ((left_border + right_border) % 2) == 1:
         right_border += 1
 
-    return (left_border, right_border)
+    return video.crop(x1=left_border, x2=clip.size[0]-right_border)
 
 
-for fname in os.listdir(TEMP_CLIPS):
-    clip = VideoFileClip(os.path.join(TEMP_CLIPS, fname))
-    # get border lengths
-    (left_border, right_border) = getBorders(clip)
-    print("left border (correct one) is " + str(left_border))
-    print("right border is " + str(right_border))
-    # base_frame = base_frame.crop((max_border, 0, base_frame.width-max_border, base_frame.height))
-    # base_frame.save(os.path.join(TEMP_CLIPS, "0-trimmed.png"))
-    trimmed_clip = clip.crop(x1=left_border, x2=clip.size[0]-right_border)
-    trimmed_clip.write_videofile(os.path.join(TEMP_CLIPS, "zzzzzzzz/" + fname[:-4] + "-trimmed.mp4"))
+# read in clips list
+with open(CLIP_PKL, 'rb') as f:
+    vid_list = pickle.load(f)
+# read in audio list
+with open(AUD_PKL, 'rb') as f:
+    aud_list = pickle.load(f)
+
+# reading from head, construct video until duration is target
+vid_clips = []
+aud_clips = []
+# holds current duration in seconds
+vid_dur = 0
+aud_dur = 0
+
+# constructs video of at least given length
+while (vid_dur < 600):
+    clip = VideoFileClip(vid_list.pop(0)[1])
+    trimmed_clip = getBorders(clip)
+    # add duration to runtime
+    vid_dur += trimmed_clip.duration
+    vid_clips.append(trimmed_clip)
+
+# make video
+output_vid = concatenate_videoclips(vid_clips, method="compose")
+
+# constructs audio backing that fits at least the vid length
+while (aud_dur < vid_dur):
+    aud_clip = AudioFileClip(aud_list.pop(0)[1])
+    aud_dur += aud_clip.duration
+    aud_clips.append(aud_clip)
+
+output_vid = output_vid.fadeout(1)
+
+# combine video and audio
+output_vid.audio = None
+output_path = os.path.join(TEMP_CLIPS, "new_vid.mp4")
+output_vid.write_videofile(output_path)
+
+background_vid = VideoFileClip(os.path.join(TEMP_CLIPS, "background.mp4"))
+background_vid = background_vid.set_duration(output_vid.duration)
+output_vid = CompositeVideoClip([background_vid, output_vid.set_position("center")])
+output_aud = concatenate_audioclips(aud_clips)
+# trim audio to fit video and re-add fade-out
+output_aud = output_aud.subclip(0,vid_dur)
+# normalize audio
+output_aud = output_aud.audio_normalize()
+# make a bit quieter since is pretty loud by default
+output_aud = output_aud.volumex(0.6)
+output_aud = output_aud.audio_fadeout(7)
+# combine video and audio
+output_vid.audio = output_aud
+output_path = os.path.join(TEMP_CLIPS, "new_vid_backed.mp4")
+output_vid.write_videofile(output_path)
